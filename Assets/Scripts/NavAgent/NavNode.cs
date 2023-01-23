eb6c891ea24a49619da62f177a7967aeb859e9b1
OliverQueenEpicMoments <jguerradominguez@student.neumont.edu>
2023-01-23 12:00:55 -0700

    Waypoint adder script thingy

--- Assets/Scripts/NavAgent/NavNode.cs
+++ Assets/Scripts/NavAgent/NavNode.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NavNode : MonoBehaviour {
-    public static NavNode[] GetNodes() {
+	[SerializeField] public List<NavNode> Neighbors = new List<NavNode>();
+	[SerializeField, Range(1, 10)] public float Radius = 1;
+
+	private void OnValidate() {
+		GetComponent<SphereCollider>().radius = Radius;
+	}
+
+	public static NavNode[] GetNodes() {
         return FindObjectsOfType<NavNode>();
     }
 
@@ -11,4 +18,14 @@ public class NavNode : MonoBehaviour {
         var Nodes = GetNodes();
         return (Nodes == null) ? null : Nodes[Random.Range(0, Nodes.Length)];
     }
+
+	private void OnDrawGizmosSelected() {
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireSphere(transform.position, Radius);
+
+		Gizmos.color = Color.green;
+		foreach (NavNode Node in Neighbors) {
+			Gizmos.DrawLine(transform.position, Node.transform.position);
+		}
+	}
 }
--- Assets/Scripts/NavAgent/WaypointNavNode.cs
+++ Assets/Scripts/NavAgent/WaypointNavNode.cs
@@ -4,36 +4,20 @@ using UnityEngine;
 
 [RequireComponent(typeof(SphereCollider))]
 public class WaypointNavNode : NavNode {
-    [SerializeField] private NavNode[] Nodes;
-    [SerializeField, Range(1, 10)] private float Radius = 1;
-
-    private void OnValidate() {
-        GetComponent<SphereCollider>().radius = Radius;
-    }
 
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.TryGetComponent<NavAgent>(out NavAgent NavAgent)) {
-            if (NavAgent.TargetNode == this) {
-                NavAgent.TargetNode = Nodes[Random.Range(0, Nodes.Length)];
+            if (NavAgent.TargetNode == this && Neighbors.Count > 0) {
+                NavAgent.TargetNode = Neighbors[Random.Range(0, Neighbors.Count)];
             }
         }
     }
 
     private void OnTriggerStay(Collider other) {
         if (other.gameObject.TryGetComponent<NavAgent>(out NavAgent NavAgent)) {
-            if (NavAgent.TargetNode == this) {
-                NavAgent.TargetNode = Nodes[Random.Range(0, Nodes.Length)];
+            if (NavAgent.TargetNode == this && Neighbors.Count > 0) {
+                NavAgent.TargetNode = Neighbors[Random.Range(0, Neighbors.Count)];
             }
-        }
-    }
-
-    private void OnDrawGizmosSelected() {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, Radius);
-
-        Gizmos.color = Color.green;
-        foreach (NavNode Node in Nodes) {
-            Gizmos.DrawLine(transform.position, Node.transform.position);
         }
     }
 }
