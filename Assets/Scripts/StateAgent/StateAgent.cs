6dd1254917c4f9a3988cf13f362a8139a0f0aa82
OliverQueenEpicMoments <jguerradominguez@student.neumont.edu>
2023-01-31 19:46:56 -0700

    Did animation stuff.

---
+++ Assets/Scripts/StateAgent/StateAgent.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateAgent : Agent {
+    [SerializeField] private Animator animator;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        if (Input.GetKey(KeyCode.Space)) {
+            animator.SetFloat("Speed", 0.5f);
+        } else {
+			animator.SetFloat("Speed", 0);
+		}
+    }
+}
